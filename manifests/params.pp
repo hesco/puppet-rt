29121e0731562b5b5859d2604d08c8f4e690e31e
Hugh Esco <hesco@campaignfoundations.com>
2014-09-21 14:49:41 -0400

    Adds Debian support, RT install w/ postgresql tested trivially

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,41 @@
 #
 class rt::params {
     case $::osfamily {
+        "Debian":  {
+            $rt_name        = 'request-tracker4'
+            $rt_dir         = "/etc/request-tracker4"
+            $rt_mailgate    = "rt4-mailgate"
+            $rt_grp         = "www-data"
+            $rt_db_mysql    = "rt4-db-mysql"
+            $rt_db_oracle   = "rt4-db-oracle"
+            $rt_db_postgres = "rt4-db-postgresql"
+            $rt_db_sqlite   = "rt4-db-sqlite"
+            $rt_tool_cnf    = "/root/.rtrc"
+            $manage_repo    = true
+            $rt4_fcgi	    = 'rt4-fcgi'
+            #
+            $ext_packages   = {
+                'assettracker'          => 'rt4-extension-assettracker',
+                'externalauth'          => 'rt4-extension-authenexternalauth',
+                'calendar' 		=> 'rt4-extension-calendar',
+                'customfieldonupdate'   => 'rt4-extension-customfieldsonupdate',
+                'jsgantt'               => 'rt4-extension-jsgantt',
+		'spawnlinkedticket'	=> 'rt4-extension-spawnlinkedticketinqueue',
+              # 'assets'                => 'perl-RT-Extension-Assets',
+              # 'activityreports'       => 'perl-RT-Extension-ActivityReports',
+              # 'quickupdate'           => 'perl-RT-Extension-QuickUpdate',
+              # 'sla'                   => 'perl-RT-Extension-SLA',
+              # 'commandbymail'         => 'perl-RT-Extension-CommandByMail',
+              # 'nagios'                => 'perl-RT-Extension-Nagios',
+              # 'cookieauth'            => 'perl-RT-Authen-CookieAuth',
+              # 'ldapimport'            => 'perl-RT-Extension-LDAPImport',
+              # 'mergeusers'            => 'perl-RT-Extension-MergeUsers',
+              # 'mobileui'              => 'perl-RT-Extension-MobileUI',
+              # 'repliestoresolved'     => 'perl-RT-Extension-RepliesToResolved',
+              # 'notifyowners'          => 'perl-RT-Extension-NotifyOwners',
+              # 'priorityasstring'      => 'perl-RT-Extension-PriorityAsString',
+            }
+        }
         "Suse":  {
             $rt_name        = 'request-tracker'
             $rt_dir         = "/etc/request-tracker"
@@ -47,6 +82,7 @@ class rt::params {
     if ($manage_repo) {
         case $::osfamily {
             "Suse":     { class { "rt::repo::zypprepo": } }
+            "Debian":   { class { "rt::repo::apt_backports": } }
             default:    { fail("Unsupported osfamily: ${::osfamily} for module ${module_name}") }
         }
     }
